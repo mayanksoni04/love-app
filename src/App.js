f1da5ddbf8324689e8697bbb730aa1f8c047b604
Mayank Soni <mayankdz1112@gmail.com>
2024-01-01 22:02:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import useSound from "use-sound";
+import "./App.css";
+import { useState } from "react";
+import gif1 from "./gif1.gif";
+import gif2 from "./gif2.gif";
+import saudebazzi from "./saudebazzi.mp3";
 function App() {
+  const audio = new Audio(saudebazzi);
+  audio.loop = true;
+  const [no, setno] = useState(55);
+  const [first, setfirst] = useState(66);
+  const [yes, setyes] = useState(true);
+  const handleno = (e) => {
+    console.log(window.innerHeight);
+    setno(Math.floor(Math.random() * 200));
+    setfirst(Math.floor(Math.random() * 200));
+
+    console.log(no);
+    e.target.style.top = no + "px";
+    e.target.style.left = first + "px";
+  };
+  const handleyes = () => {
+    setyes(false);
+    audio.loop = true;
+    audio.play();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {yes ? (
+          <div className="parent">
+            <div className="child">
+              <h1>Do U Love ME ; )</h1>
+              <div className="child1">
+                <img src={gif1} alt="" />
+              </div>
+              <div className="buttonp">
+                <button onClick={handleyes} className="btn1">
+                  Yes
+                </button>
+                <button
+                  onClick={handleno}
+                  onMouseOver={handleno}
+                  className="btn2"
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="parent">
+            <div className="child">
+              <h1>I Love U 2</h1>
+              <div className="child1">
+                <img src={gif2} alt="" />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
